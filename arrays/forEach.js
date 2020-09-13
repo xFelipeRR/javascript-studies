e572f102d3211eba88de59ed18ff2aa90d1bd213
Felipe Rangel Ribeiro <lipefortaleza2008@hotmail.com>
2020-09-13 09:14:10 -0300

    propriedades de arrays

---
+++ arrays/forEach.js
@@ -0,0 +1,6 @@
+var nomes = ["Felipe", "Douglas", "João"]
+
+nomes.forEach((nome, indice) => {
+    console.log(indice + "-" + nome)
+})
+// retorna todos os elementos do array e o indice(1° e 2° parametro)
---
+++ arrays/pop.js
@@ -0,0 +1,7 @@
+var nomes = []
+
+nomes.push("Felipe")
+nomes.push("João")
+nomes.push("Ester")
+nomes.push("José")
+// a funcionalidade pop retira do array o ultimo elemento antes da chamada do pop
